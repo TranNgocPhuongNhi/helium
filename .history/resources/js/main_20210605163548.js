b2b52bc41e8df123e5c4fe76e353aa536dae680e
Trần Ngọc Phương Nhi <tranngocphuongnhi0412@gmail.com>
2021-06-09 11:32:44 +0700

    new update

---
+++ .history/resources/js/main_20210605163548.js
@@ -0,0 +1,17 @@
+
+//============================= Scrolling Navigation Bar =============================================================
+const nav = document.querySelector("nav");
+window.addEventListener("scroll",() => {
+    const offset = window.pageYOffset;
+    if(offset > 20) {
+        nav.classList.add('scroll','animate__animated', 'animate__fadeInDown');
+    }
+    else {
+        nav.classList.remove('scroll');
+    }
+})
+
+// ========================================= Owl Carousel =====================================
+$('#owl-carousel').owlCarousel({
+    
+});
